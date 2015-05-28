2055ee5ab500db318e00351a2bc2e5a6b55b147c
zing.wang <zing.wang@outlook.com>
2015-05-28 23:52:21 +0800

    add config script

--- docs/setting.sql
+++ docs/setting.sql
@@ -11,8 +11,82 @@
 
 USE `copartner`;
 
+TRUNCATE `location`;
+INSERT INTO `location` (`id`, `parent_id`, `name`, `pinyin`, `is_listed`, `created`) VALUES
+(1, 0, '北京', 'beijing', 1, NOW()),
+(2, 0, '上海', 'shanghai', 1, NOW()),
+(3, 0, '天津', 'tianjin', 1, NOW()),
+(4, 0, '重庆', 'chongqing', 1, NOW()),
+(5, 0, '辽宁', 'liaoning', 1, NOW()),
+(6, 0, '吉林', 'jilin', 1, NOW()),
+(7, 0, '黑龙江', 'heilongjiang', 1, NOW()),
+(8, 0, '河北', 'hebei', 1, NOW()),
+(9, 0, '山西', 'shanxi', 1, NOW()),
+(10, 0, '陕西', 'shanxi', 1, NOW()),
+(11, 0, '甘肃', 'gansu', 1, NOW()),
+(12, 0, '青海', 'qinghai', 1, NOW()),
+(13, 0, '山东', 'shandong', 1, NOW()),
+(14, 0, '安徽', 'anhui', 1, NOW()),
+(15, 0, '江苏', 'jiangsu', 1, NOW()),
+(16, 0, '浙江', 'zhejiang', 1, NOW()),
+(17, 0, '河南', 'henan', 1, NOW()),
+(18, 0, '湖北', 'hubei', 1, NOW()),
+(19, 0, '湖南', 'hunan', 1, NOW()),
+(20, 0, '江西', 'jiangxi', 1, NOW()),
+(21, 0, '福建', 'fujian', 1, NOW()),
+(22, 0, '云南', 'yunnan', 1, NOW()),
+(23, 0, '海南', 'hannan', 1, NOW()),
+(24, 0, '四川', 'sichuan', 1, NOW()),
+(25, 0, '贵州', 'guizhou', 1, NOW()),
+(26, 0, '广东', 'guangdong', 1, NOW()),
+(27, 0, '内蒙古', 'neimenggu', 1, NOW()),
+(28, 0, '新疆', 'xinjiang', 1, NOW()),
+(29, 0, '广西', 'guangxi', 1, NOW()),
+(30, 0, '西藏', 'xizhang', 1, NOW()),
+(31, 0, '宁夏', 'ningxia', 1, NOW()),
+(32, 0, '香港', 'xianggang', 1, NOW()),
+(33, 0, '澳门', 'aomen', 1, NOW()),
+(34, 0, '台湾', 'taiwan', 1, NOW()),
+(35, 1, '北京', 'beijing', 1, NOW()),
+(36, 2, '上海', 'shanghai', 1, NOW()),
+(37, 3, '天津', 'tianjin', 1, NOW()),
+(38, 4, '重庆', 'chongqing', 1, NOW());
+
+TRUNCATE `industry_domain`;
+INSERT INTO `industry_domain` (`id`, `name`, `is_listed`, `created`) VALUES
+(1, '互联网', 1, NOW()),
+(2, '移动互联网', 1, NOW()),
+(3, '电子商务', 1, NOW()),
+(4, '社交', 1, NOW()),
+(5, '游戏', 1, NOW()),
+(6, '云计算', 1, NOW()),
+(7, 'IT服务', 1, NOW()),
+(8, '软件/工具', 1, NOW()),
+(9, '软硬件', 1, NOW()),
+(10, '数码电子', 1, NOW()),
+(11, '其它', 1, NOW());
+
+TRUNCATE `startup_role`;
+INSERT INTO `startup_role` (`id`, `name`, `is_listed`, `created`) VALUES
+(1, '产品', 1, NOW()),
+(2, '技术', 1, NOW()),
+(3, '运营', 1, NOW()),
+(4, '营销', 1, NOW()),
+(5, '设计', 1, NOW()),
+(6, '其它', 1, NOW());
+
+TRUNCATE `startup_status`;
+INSERT INTO `startup_status` (`id`, `name`, `is_listed`, `created`) VALUES
+(1, '有创业意愿', 1, NOW()),
+(2, '有创业想法', 1, NOW()),
+(3, '全职创业中', 1, NOW()),
+(4, '兼职创业中', 1, NOW()),
+(5, '投资人', 1, NOW()),
+(6, '媒体人', 1, NOW());
+
 TRUNCATE `system_setting`;
-INSERT INTO `system_setting` (`id`, `group`,`key`,`value`,`created`,`updated`) VALUES (1,'config','config_smtp_host','smtp.163.com',NOW(),NULL),
+INSERT INTO `system_setting` (`id`, `group`,`key`,`value`,`created`,`updated`) VALUES
+(1,'config','config_smtp_host','smtp.163.com',NOW(),NULL),
 (2,'config','config_smtp_port','25',NOW(),NULL),
 (3,'config','config_smtp_username','ec_java@163.com',NOW(),NULL),
 (4,'config','config_smtp_password','abc123_',NOW(),NULL),
@@ -30,7 +104,7 @@ INSERT INTO `system_setting` (`id`, `group`,`key`,`value`,`created`,`updated`) V
 (16,'image','file_dimension_max_height','1024',NOW(),NULL),
 (17,'image','file_dimension_min_width','50',NOW(),NULL),
 (18,'image','file_dimension_min_height','50',NOW(),NULL);
-    
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
