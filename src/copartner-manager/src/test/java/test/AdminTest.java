2c5642cf34dd672902ebcb1a217343dd15dda36c
liufeiit <liufei_it@126.com>
2015-09-22 22:56:21 +0800

    Test

---
+++ src/copartner-manager/src/test/java/test/AdminTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.Test;
+
+import com.insoul.copartner.domain.Admin;
+import com.insoul.copartner.utils.Permission;
+
+/**
+ * @author 刘飞 E-mail:liufei_it@126.com
+ *
+ * @version 1.0.0
+ * @since 2015年9月22日 下午10:54:10
+ */
+public class AdminTest {
+
+    @Test
+    public void test() {
+        Admin admin = new Admin();
+        admin.addPermission(Permission.SuperAdmin).addPermission(Permission.Admin).addPermission(Permission.User);
+        System.out.println(admin.getPermission());
+    }
+}
