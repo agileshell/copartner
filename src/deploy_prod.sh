26491c2e68dc78e2b9564da9eff88229e8f6af5f
liufeiit <liufei_it@126.com>
2015-09-30 15:37:36 +0800

    deploy shell

--- src/deploy_prod.sh
+++ src/deploy_prod.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 mvn clean && mvn install
 echo "build success."
-rsync -a8P --delete copartner-manager/target/copartner-manager/* root@copartner:/root/app/mgr/webapps/ROOT/
-echo "sync mgr success."
-#rsync -a8P --delete copartner-api/target/copartner-api/* root@copartner:/root/app/api/webapps/ROOT/
-echo "sync api success."
+option="${1}"
+echo "deploy ${option}"
+case ${option} in
+   -api)
+		rsync -a8P --delete copartner-api/target/copartner-api/* root@copartner:/root/app/api/webapps/ROOT/
+		echo "sync api success."
+      ;;
+   -mgr)
+		rsync -a8P --delete copartner-manager/target/copartner-manager/* root@copartner:/root/app/mgr/webapps/ROOT/
+		echo "sync mgr success."
+      ;;
+   -all) 
+		rsync -a8P --delete copartner-api/target/copartner-api/* root@copartner:/root/app/api/webapps/ROOT/
+		echo "sync api success."
+		rsync -a8P --delete copartner-manager/target/copartner-manager/* root@copartner:/root/app/mgr/webapps/ROOT/
+		echo "sync mgr success."
+      ;;
+esac
 
 
